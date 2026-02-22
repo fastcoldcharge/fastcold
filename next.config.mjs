db2fd7463a0762fc4e5838862b431f6636e3af0c
FAST-COLD <fastcoldcharge@gmail.com>
2026-02-22 01:45:43 -0500

    Update next.config.mjs for production settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
+const repo = "fastcold"; // <-- pon aquí el nombre exacto del repo
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
 };
 
 export default nextConfig;
